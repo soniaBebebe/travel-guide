54ece7f891c5ca74fa9ac6e14b5e4d4c510a3891
soniaBebebe <sonctonc@gmail.com>
2026-08-16 21:53:55 -0700

    bebebe

---
+++ travel-planner/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className="container">
+        <h1>Travel Planner</h1>
+        <p className="meta"> Каркас работает</p>
+    </div>
+  )
+}

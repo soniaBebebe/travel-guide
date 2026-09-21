822b75c9973f46e1e1ea575282fe6be23c554b38
soniaBebebe <sonctonc@gmail.com>
2026-09-20 21:55:59 -0700

    bebebe

--- travel-planner/src/shared/API/supabase.ts
+++ travel-planner/src/shared/API/supabase.ts
@@ -3,7 +3,7 @@ import {createClient} from '@supabase/supabase-js';
 const url= import.meta.env.VITE_SUPABASE_URL;
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if(!url || key){
+if(!url || !key){
     throw new Error ('Не заданы VITE_SUPABASE_URL/VITE_SUPABASE_ANON_KEY в .env');
 }
 
